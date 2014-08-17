680a641d80e911acd2c7734d4c1f98d1642926b3
Johannes Donath <johannesd@evil-co.com>
2014-08-18 00:21:09 +0200

    Added a basic brain.

---
+++ src/main/java/org/evilco/bot/powersweeper/brain/IBrain.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2014 Johannes Donath <johannesd@evil-co.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evilco.bot.powersweeper.brain;
+
+import org.evilco.bot.powersweeper.game.IChunk;
+import org.evilco.bot.powersweeper.game.IGameInterface;
+
+/**
+ * @author Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.com>
+ */
+public interface IBrain {
+
+	/**
+	 * Performs a single game "turn".
+	 * @param chunk The current chunk.
+	 * @param gameInterface The game interface.
+	 */
+	public void think (IChunk chunk, IGameInterface gameInterface);
+}
