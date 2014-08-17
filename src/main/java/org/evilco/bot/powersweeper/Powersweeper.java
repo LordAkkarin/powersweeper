874e7be7af609d86cbcee8a3f48c5d92a0713d25
Johannes Donath <johannesd@evil-co.com>
2014-08-17 21:05:18 +0200

    Added main class ...

---
+++ src/main/java/org/evilco/bot/powersweeper/Powersweeper.java
@@ -0,0 +1,16 @@
+package org.evilco.bot.powersweeper;
+
+/**
+ * @author Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.com>
+ */
+public class Powersweeper {
+
+	/**
+	 * Main Entry Point
+	 * @param arguments The arguments.
+	 */
+	public static void main (String[] arguments) {
+
+	}
+}
