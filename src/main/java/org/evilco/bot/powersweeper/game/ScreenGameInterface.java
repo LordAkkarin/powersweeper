cee87e80348f425eb93761b854ca6e460736fcb1
Johannes Donath <johannesd@evil-co.com>
2014-08-19 09:43:51 +0200

    Added a simple parser implementation.

--- src/main/java/org/evilco/bot/powersweeper/game/ScreenGameInterface.java
+++ src/main/java/org/evilco/bot/powersweeper/game/ScreenGameInterface.java
@@ -15,6 +15,7 @@
 
 package org.evilco.bot.powersweeper.game;
 
+import com.google.common.collect.ImmutableMap;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NonNull;
@@ -30,7 +31,11 @@ import org.openqa.selenium.interactions.Actions;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Map;
+import java.util.Stack;
 
 /**
  * @author Johannes Donath <johannesd@evil-co.com>
@@ -44,9 +49,19 @@ public class ScreenGameInterface implements IGameInterface {
 	public static final short CELL_SIZE = 32;
 
 	/**
+	 * Stores the bomb tile.
+	 */
+	public static final BufferedImage TILE_BOMB;
+
+	/**
+	 * Stores the tile map.
+	 */
+	public static final Map<Short, BufferedImage> TILE_MAP;
+
+	/**
 	 * Stores the current chunk instance.
 	 */
-	private IChunk currentChunk;
+	private IChunk currentChunk = null;
 
 	/**
 	 * Stores the current chunk X-Coordinate.
@@ -76,11 +91,49 @@ public class ScreenGameInterface implements IGameInterface {
 	private final Powersweeper parent;
 
 	/**
+	 * Static Initialization
+	 */
+	static {
+		// create builder
+		ImmutableMap.Builder<Short, BufferedImage> tileBuilder = new ImmutableMap.Builder<> ();
+
+		// fill with known values
+		for (short i = 1; i <= 8; i++) {
+			try {
+				tileBuilder.put (i, ImageIO.read (ScreenGameInterface.class.getResourceAsStream ("/tile/number-" + i + ".png")));
+			} catch (Exception ex) {
+				getLogger ().warn ("Could not load tile for number " + i + ": " + ex.getMessage (), ex);
+			}
+		}
+
+		// load bomb tile
+		BufferedImage bomb = null;
+
+		try {
+			bomb = ImageIO.read (ScreenGameInterface.class.getResourceAsStream ("/tile/bomb.png"));
+		} catch (Exception ex) {
+			getLogger ().warn ("Could not load bomb tile: " + ex.getMessage (), ex);
+		}
+
+		// store tile
+		TILE_BOMB = bomb;
+
+		// build map
+		TILE_MAP = tileBuilder.build ();
+	}
+
+	/**
 	 * Constructs a new CanvasGameInterface instance.
 	 * @param parent The parent application.
 	 */
 	public ScreenGameInterface (@NonNull Powersweeper parent) {
+		getLogger ().entry ();
+
+		// store arguments
 		this.parent = parent;
+
+		// trace
+		getLogger ().exit ();
 	}
 
 	/**
@@ -88,8 +141,11 @@ public class ScreenGameInterface implements IGameInterface {
 	 */
 	@Override
 	public boolean flagField (short x, short y) {
-		if (this.currentChunk == null) return false;
-		if (this.currentChunk.getField (x, y) != FieldState.UNTOUCHED) return false;
+		getLogger ().entry ();
+
+		// validate
+		if (this.currentChunk == null) return getLogger ().exit (false);
+		if (this.currentChunk.getField (x, y) != FieldState.UNTOUCHED) return getLogger ().exit (false);
 
 		// find HTML coordinate
 		WebElement html = this.parent.getDriverManager ().getDriver ().findElement (By.name ("html"));
@@ -104,7 +160,33 @@ public class ScreenGameInterface implements IGameInterface {
 
 		// update cache
 		this.currentChunk.setField (x, y, FieldState.FLAGGED);
-		return true;
+		return getLogger ().exit (true);
+	}
+
+	/**
+	 * Returns a pixel value.
+	 * @param x The X-Coordinate.
+	 * @param y The Y-Coordinate.
+	 * @return The pixel color.
+	 */
+	public Color getPixel (int x, int y) {
+		getLogger ().entry ();
+
+		// decode color
+		return getLogger ().exit (new Color (this.getRawPixel (x, y)));
+	}
+
+	/**
+	 * Returns the raw pixel value.
+	 * @param x The X-Coordinate.
+	 * @param y The Y-Coordinate.
+	 * @return The color value.
+	 */
+	public int getRawPixel (int x, int y) {
+		getLogger ().entry ();
+
+		// get value
+		return getLogger ().exit ((this.image.getRGB (x, y) & 0x00FFFFFF)); // drop alpha bit
 	}
 
 	/**
@@ -113,7 +195,143 @@ public class ScreenGameInterface implements IGameInterface {
 	 * @return The real coordinate.
 	 */
 	public short getRealCoordinate (short in) {
-		return ((short) ((in * CELL_SIZE) + Math.floor ((CELL_SIZE / 2))));
+		getLogger ().entry ();
+
+		// calculate
+		return getLogger ().exit (((short) ((in * CELL_SIZE) + Math.floor ((CELL_SIZE / 2)))));
+	}
+
+	/**
+	 * Guesses a field state.
+	 * @param fieldX The X-Coordinate.
+	 * @param fieldY The Y-Coordinate.
+	 */
+	public void guessFieldState (short fieldX, short fieldY) {
+		getLogger ().entry ();
+
+		// calculate absolute position
+		int x = (fieldX * 32);
+		int y = (fieldY * 32);
+
+		// extract tile
+		BufferedImage tile = this.image.getSubimage (x, y, 30, 30);
+
+		// calculate average color
+		int averageR = 0;
+		int averageG = 0;
+		int averageB = 0;
+
+		for (int currentX = 0; currentX < tile.getWidth (); currentX++) {
+			for (int currentY = 0; currentY < tile.getHeight (); currentY++) {
+				// parse color
+				Color color = new Color (tile.getRGB (currentX, currentY));
+
+				// add to average
+				averageR += color.getR ();
+				averageG += color.getG ();
+				averageB += color.getB ();
+			}
+		}
+
+		averageR /= 900;
+		averageG /= 900;
+		averageB /= 900;
+
+		// re-create color
+		Color average = new Color (averageR, averageG, averageB);
+
+		// check for obvious values
+		// Untouched field
+		if (average.getValue () == 0xDBDBDB) {
+			// update cache
+			this.currentChunk.setField (fieldX, fieldY, FieldState.UNTOUCHED);
+			this.currentChunk.setValue (fieldX, fieldY, ((short) -1));
+
+			// trace
+			getLogger ().trace ("Set value for field " + fieldX + "," + fieldY + " to UNTOUCHED:-1.");
+
+			// skip further execution
+			return;
+		}
+
+		// Flag
+		if (average.getValue () == 0xC6B7B5) {
+			// update cache
+			this.currentChunk.setField (fieldX, fieldY, FieldState.FLAGGED);
+			this.currentChunk.setValue (fieldX, fieldY, ((short) -1));
+
+			// trace
+			getLogger ().trace ("Set value for field " + fieldX + "," + fieldY + " to FLAGGED:-1.");
+
+			// skip further execution
+			return;
+		}
+
+		// check whether color average is within known bounds
+		if (average.isGray ()) {
+			// log
+			getLogger ().trace ("Detected a perfect gray value. Checking for number tiles.");
+
+			// guess number
+			short number = this.guessNumber (tile, average);
+
+			// store data
+			if (number >= 0) {
+				// update cache
+				this.currentChunk.setField (fieldX, fieldY, FieldState.NUMBER);
+				this.currentChunk.setValue (fieldX, fieldY, number);
+
+				// trace
+				getLogger ().trace ("Set value for field " + fieldX + "," + fieldY + " to NUMBER:" + number + ".");
+
+				// skip further execution
+				return;
+			}
+
+			// check for bombs
+			if (this.tileMatches (tile, TILE_BOMB)) {
+				// update cache
+				this.currentChunk.setField (fieldX, fieldY, FieldState.BOMB);
+				this.currentChunk.setValue (fieldX, fieldY, ((short) -1));
+
+				// trace
+				getLogger ().trace ("Set value for field " + fieldX + "," + fieldY + " to BOMB:-1.");
+
+				// skip further execution
+				return;
+			}
+		}
+
+		// warn
+		getLogger ().debug ("Could not guess value for field " + fieldX + "," + fieldY + ". Assuming flagged.");
+
+		// set value to flagged
+		this.currentChunk.setField (fieldX, fieldY, FieldState.FLAGGED);
+		this.currentChunk.setValue (fieldX, fieldY, ((short) -1));
+
+		// trace
+		getLogger ().exit ();
+	}
+
+	/**
+	 * Guesses a tile number.
+	 * @param tile The tile.
+	 * @param average The average color.
+	 * @return The tile number (-1 if unknown).
+	 */
+	public short guessNumber (@NonNull BufferedImage tile, @NonNull Color average) {
+		getLogger ().entry ();
+
+		// check for empty field
+		if (average.getValue () == 0xFFFFFF) return 0;
+
+		// check numbers
+		for (Map.Entry<Short, BufferedImage> preset : TILE_MAP.entrySet ()) {
+			if (this.tileMatches (tile, preset.getValue ())) return preset.getKey ();
+		}
+
+		// return unknown value
+		return getLogger ().exit (((short) -1));
 	}
 
 	/**
@@ -121,6 +339,8 @@ public class ScreenGameInterface implements IGameInterface {
 	 */
 	@Override
 	public void move (long x, long y) {
+		getLogger ().entry ();
+
 		// reset chunk
 		this.currentChunk = null;
 
@@ -130,6 +350,32 @@ public class ScreenGameInterface implements IGameInterface {
 		// update position
 		this.currentChunkX = x;
 		this.currentChunkY = y;
+
+		// trace
+		getLogger ().exit ();
+	}
+
+	/**
+	 * Checks whether two tiles match exactly.
+	 * @param i1 Tile 1.
+	 * @param i2 Tile 2.
+	 * @return True if both tiles are exactly the same.
+	 */
+	public boolean tileMatches (@NonNull BufferedImage i1, @NonNull BufferedImage i2) {
+		// check size
+		if (i1.getWidth () != i2.getWidth ()) return false;
+		if (i1.getHeight () != i2.getHeight ()) return false;
+
+		// check pixels
+		for (int x = 0; x < i1.getWidth (); x++) {
+			for (int y = 0; y < i1.getHeight (); y++) {
+				// check pixel
+				if (i1.getRGB (x, y) != i2.getRGB (x, y)) return false;
+			}
+		}
+
+		// everything matches
+		return true;
 	}
 
 	/**
@@ -137,15 +383,25 @@ public class ScreenGameInterface implements IGameInterface {
 	 */
 	@Override
 	public IChunk update () {
+		getLogger ().entry ();
+
 		// create matrix
-		if (this.currentChunk == null) new MatrixChunk (this.currentChunkX, this.currentChunkY, ((short) 20), ((short) 20));
+		if (this.currentChunk == null) this.currentChunk = new MatrixChunk (this.currentChunkX, this.currentChunkY, ((short) 20), ((short) 20));
+
+		// focefully close all popups
+		this.parent.getDriverManager ().getExecutor ().executeScript ("$('.popup').hide ();");
 
 		// parse
 		try {
 			// get current display
 			this.image = ImageIO.read (new ByteArrayInputStream (((TakesScreenshot) this.parent.getDriverManager ().getDriver ()).getScreenshotAs (OutputType.BYTES)));
 
-			// TODO: Parse screen
+			// parse screen
+			for (short x = 0; x < 20; x++) {
+				for (short y = 0; y < 20; y++) {
+					this.guessFieldState (x, y);
+				}
+			}
 		} catch (IOException ex) {
 			// warn user as we're not updating our data this turn
 			// this will cause unexpected behaviour
@@ -153,7 +409,7 @@ public class ScreenGameInterface implements IGameInterface {
 		}
 
 		// return parsed chunk
-		return this.currentChunk;
+		return getLogger ().exit (this.currentChunk);
 	}
 
 	/**
@@ -161,8 +417,11 @@ public class ScreenGameInterface implements IGameInterface {
 	 */
 	@Override
 	public boolean uncoverField (short x, short y) {
-		if (this.currentChunk == null) return false;
-		if (this.currentChunk.getField (x, y) != FieldState.UNTOUCHED);
+		getLogger ().entry ();
+
+		// verify
+		if (this.currentChunk == null) getLogger ().exit (false);
+		if (this.currentChunk.getField (x, y) != FieldState.UNTOUCHED) return getLogger ().exit (false);
 
 		// find HTML coordinate
 		WebElement html = this.parent.getDriverManager ().getDriver ().findElement (By.name ("html"));
@@ -177,6 +436,6 @@ public class ScreenGameInterface implements IGameInterface {
 
 		// update cache
 		this.currentChunk.setField (x, y, FieldState.UNCOVERED);
-		return true;
+		return getLogger ().exit (true);
 	}
 }
