938d2828490d0508a1a48e670d7bf69b542e6db3
Johannes Donath <johannesd@evil-co.com>
2014-08-19 12:26:55 +0200

    Implemented brain for dumping unknown tiles.

---
+++ src/main/java/org/evilco/bot/powersweeper/brain/DumpBrain.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2014 Johannes Donath <johannesd@evil-co.com>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.evilco.bot.powersweeper.brain;
+
+import lombok.Getter;
+import lombok.NonNull;
+import org.evilco.bot.powersweeper.game.IChunk;
+import org.evilco.bot.powersweeper.game.IGameInterface;
+
+/**
+ * @author Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.com>
+ */
+public class DumpBrain extends IdiotBrain {
+
+	/**
+	 * Stores the maximum amount of moves performed until the bot will move on.
+	 */
+	public static final int THRESHOLD = 150;
+
+	/**
+	 * Stores the click counter.
+	 */
+	@Getter
+	private int counter = 0;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void think (@NonNull IChunk chunk, @NonNull IGameInterface gameInterface) {
+		super.think (chunk, gameInterface);
+
+		// update counter
+		this.counter++;
+
+		// log
+		getLogger ().debug (this.counter + " moves out of " + THRESHOLD + " performed.");
+
+		// check
+		if (this.counter >= THRESHOLD) {
+			// log
+			getLogger ().info ("Finished current field. Moving on.");
+
+			// move field
+			gameInterface.move ((chunk.getChunkX () + 1), chunk.getChunkY ());
+
+			// reset counter
+			this.counter = 0;
+		}
+	}
+}
